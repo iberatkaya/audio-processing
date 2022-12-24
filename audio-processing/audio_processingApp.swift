f5fc8749b55f1c4dcd80c4ef1edccb0e726734e2
iberatkaya <beratkaya1998@gmail.com>
2022-12-23 19:34:06 -0600

    Initial Commit

---
+++ audio-processing/audio_processingApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_processingApp.swift
+//  audio-processing
+//
+//  Created by Ibrahim Berat Kaya on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct audio_processingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
